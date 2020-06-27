53c06c966db6216d1419ee4038197903de00d14d
unknown <soratto.andre@gmail.com>
2020-06-27 14:12:08 -0300

    Updates

---
+++ precache-manifest.be07d615248b69a5b0c30ceac7daff07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a144c687e162aa2bb518aa135d12a713",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0b19d103fd90ea15e108",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b75f8dc177b13a3d39f7",
+    "url": "/portfolio/static/js/2.fad1c4fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.fad1c4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86492c00c8e5422f600f",
+    "url": "/portfolio/static/js/3.5019244d.chunk.js"
+  },
+  {
+    "revision": "064a9966961f7b8051a54b910a7eb515",
+    "url": "/portfolio/static/js/3.5019244d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b19d103fd90ea15e108",
+    "url": "/portfolio/static/js/main.4c919699.chunk.js"
+  },
+  {
+    "revision": "65fd252cc27124facdd2",
+    "url": "/portfolio/static/js/runtime-main.e2c457f7.js"
+  }
+]);
